056d44b11e63d12b14b8f0e88c794fdd16236907
Ben Ju <benas.juskus@gmail.com>
2021-07-08 21:51:55 +0300

    Updated

--- Classworks/06-21/app.js
+++
@@ -1,4 +0,0 @@
-let age = prompt('iveskite savo gimio metus');
-let year = new Date();
-let result = (year.getFullYear());
-document.write(`You are ${result - age} years old`);
--- Classworks/07-01/app.js
+++
@@ -1,2 +0,0 @@
-console.log (document.head.children)
-document.getElementsByTagName('meta')
