bc4f95146aa609c3ab11a895cd615ae9895127d3
Ben Ju <benas.juskus@gmail.com>
2021-07-19 10:48:51 +0300

    Homeworks 07-15

--- Classworks/07-15/index.php
+++ Classworks/07-15/index.php
@@ -1,1 +1,37 @@
 <?php
+//
+////phpinfo();
+//
+//echo 'Hello world';
+//print 'Labas pasauli';
+//
+//var_dump('Hello world');
+//
+////Variables
+//$_name = 'Benas';
+//
+////Names have to start with letter or underscore _
+//var_dump($_name);
+//
+//$_name2 = 'Juskus';
+//
+//var_dump($_name2);
+//
+//var_dump($_name . $_name2);
+
+$digit = 8;
+$letter = 'K';
+$empty = null;
+
+var_dump($digit, $letter, $empty);
+$string = 'Testing this text';
+
+var_dump(substr($string, -3));
+
+
+function negative($number)
+{
+    return $number * -1;
+}
+
+var_dump(negative(5));
